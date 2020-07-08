3f9723c6a855a4dd4982732ad26809d8e2c59a0b
HeliumSpectrum <67976720+HeliumSpectrum@users.noreply.github.com>
2020-07-08 20:41:45 +0530

    Add files via upload
    ello mate just me making fun

---
+++ sajibot.py
@@ -0,0 +1,24 @@
+a=2
+b=7
+c=10
+d=2
+e=14
+if a>b:
+    print('Guddu CHildu')
+else:
+    print('Worst Fellow')
+
+if c<b:
+    print('Guddu Childu')
+else:
+    print('Worst fellow')
+
+if a==d:
+    print('Guddu Childu')
+else:
+    print('Worst fellow')
+
+if a*b==e:
+    print('Guddu Childu')
+else:
+    print('Worst Fellow')
